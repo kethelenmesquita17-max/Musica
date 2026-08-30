d8c04f72279c0a0f3e46d899902777c8a74cc0e7
Victor hugo <victor.silva252@aluno.cps.sp.gov.br>
2026-08-30 11:33:56 -0300

    atualização do código

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("PARABÉNS PRA VOCÊ");
+Console.ResetColor();
+Thread.Sleep(1000);
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Parabéns pra você");
+Thread.Sleep(1000);
+Console.WriteLine("Nesta data querida");
+Thread.Sleep(1000);
+Console.WriteLine("Muitas felicidades");
+Thread.Sleep(1000);
+Console.WriteLine("Muitos anos de vida");
+Thread.Sleep(1000);
+Console.ResetColor();
+Console.WriteLine();
